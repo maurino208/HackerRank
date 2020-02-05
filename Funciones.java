41e45b05abdda971fe663047c97811e4c4cba29e
maurino208 <maurino208@gmail.com>
2020-02-05 20:38:14 -0300

    Agrego nuevos ejercicios

--- Funciones.java
+++ Funciones.java
@@ -1,4 +1,4 @@
-Dado un string obtiene un arraylist con los caracteres
+//Dado un string obtiene un arraylist con los caracteres
 
 static ArrayList<String> getChar(String s) {
 		ArrayList<String> arrayString=new ArrayList<String>();
@@ -7,7 +7,21 @@ static ArrayList<String> getChar(String s) {
 			if(!arrayString.contains(carac)) {
 				arrayString.add(carac);
 			}
+		}
+		return arrayString;
+}
 
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
-
+//método para calcular el número de veces que se repite un carácter en un String
+public static int contarCaracteres(String cadena, char caracter) {
+        int posicion, contador = 0;
+        //se busca la primera vez que aparece
+        posicion = cadena.indexOf(caracter); //indexOf=devuelve la primera posición dentro del String donde se encuentra el carácter o -1 si no lo ha encontrado.
+        while (posicion != -1) { //mientras se encuentre el caracter
+            contador++;           //se cuenta
+            //se sigue buscando a partir de la posición siguiente a la encontrada
+            posicion = cadena.indexOf(caracter, posicion + 1);
+        }
+        return contador;
+}
---
+++ sock_merchant.java
@@ -0,0 +1,5 @@
+package hackerrank;
+
+public class sock_merchant {
+
+}
